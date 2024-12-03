ac23994dca77d1cb60b45ffa97de550cd9dae836
ChungCr7 <baochungas3@gmail.com>
2024-12-03 12:41:34 +0700

    up

--- config/connectdb.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export function connectdb(){
-    mongoose.connect('mongodb://127.0.0.1:27017/baochungst22a')
-.then(() => console.log('Kết nối mongodb thành công!'))
-.catch(err => console.error("Connection error:", err));
-}
--- routes/root.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import Home from "../controllers/home_controller.js";
-const router = express.Router();
-
-// Goi ham
-router.get("/", Home.home);
-router.get("/about", Home.about);
-
-export default router;
